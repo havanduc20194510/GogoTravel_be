64579ff54dbeaa0b391efd66dfa2c7d381d2ef38
VanDucKun <havanducdeptrai@gmail.com>
2024-04-05 09:47:53 +0700

    code base

---
+++ src/main/java/com/haduc/vietnam_travel_system/VietnamTravelSystemApplication.java
@@ -0,0 +1,13 @@
+package com.haduc.vietnam_travel_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietnamTravelSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VietnamTravelSystemApplication.class, args);
+    }
+
+}
